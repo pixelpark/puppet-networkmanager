cb4a3a2619f411dfd0c9d52056ebfd4914288c37
Robert Waffen <robert.waffen@pixelpark.com>
2021-04-09 16:19:23 +0200

    reduce code complexity
    networkmanager_primarydevice is implemented via core fact networking.primary
    remove special fact.

--- lib/facter/networkmanager_primaryconnection.rb
+++ lib/facter/networkmanager_primaryconnection.rb
@@ -3,7 +3,7 @@ Facter.add(:networkmanager_primaryconnection) do
   setcode do
     nmcli = Facter.value('networkmanager_nmcli_path')
     if nmcli
-      device = Facter.value('networkmanager_primarydevice')
+      device = Facter.value('networking')['primary']
       if device
         cmd = "#{nmcli} -g general.connection d show #{device}"
         Facter::Core::Execution.execute(cmd)
